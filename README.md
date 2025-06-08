bcfe7e740a9397f95da86ada080ab09d6f19d220
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2025-06-08 14:55:09 -0300

    Add frontend service to Docker Compose, update README with frontend URL, and enable CORS in the backend application

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # NLP2Filters
 
+http://localhost:8080/
+
+
 http://localhost:5000/health 
 
 
 POST /embedding/echo с JSON {"text": "hello"} — job_id.
+
 GET /embedding/result/<job_id> — {"result": "Echo: hello"}.
--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from routes_embedding import embedding_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:8080"])
 app.register_blueprint(embedding_bp)
 
 
